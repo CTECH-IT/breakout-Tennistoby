7402b43b2c0a8d608b3c0e518060e3ea528edf24
Zoey Chen <8047902@isd535.org>
2021-09-13 09:26:57 -0500

    added square

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-let canvas = document.getElementbyID("myCanvas");
+let canvas = document.getElementById("myCanvas");
 let ctx = canvas.getContext("2d");
 
 ctx.beginPath();
@@ -6,7 +6,7 @@ ctx.rect(20,40,50,50);
 ctx.fillStyle = "#FF0000"; 
 ctx.fill(); 
 ctx.closePath();
-
+/*
 ctx.beginPath();
 ctx.arc(240,160,20,0,Math.PI*2, false);
 ctx.fillStyle = "#FF0000"; 
@@ -15,6 +15,8 @@ ctx.closePath();
 
 ctx.beginPath();
 ctx.rect(160,10,100,40);
-ctx.strokeStyle = "rgba(0,0,255,0.5)"
+ctx.strokeStyle = "rgba(0,0,255,0.5)";
 ctx.stroke();
 ctx.closePath();
+
+*/
