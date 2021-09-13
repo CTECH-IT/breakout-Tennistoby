6cadf2313f5e6b53bec246d9c2627fd4649ea774
Zoey Chen <8047902@isd535.org>
2021-09-13 09:46:08 -0500

    added draw function

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,32 @@
 let canvas = document.getElementById("myCanvas");
 let ctx = canvas.getContext("2d");
 
-ctx.beginPath();
-ctx.rect(20,40,50,50);
-ctx.fillStyle = "#FF0000"; 
-ctx.fill(); 
-ctx.closePath();
-/*
-ctx.beginPath();
-ctx.arc(240,160,20,0,Math.PI*2, false);
-ctx.fillStyle = "#FF0000"; 
-ctx.fill(); 
-ctx.closePath();
+let x = canvas.width/2;
+let y = canvas.height-30;
+let dx = 2;
+let dy = -2;
+let ballRadius = 10;
 
-ctx.beginPath();
-ctx.rect(160,10,100,40);
-ctx.strokeStyle = "rgba(0,0,255,0.5)";
-ctx.stroke();
-ctx.closePath();
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(x,y,10,0,ballRadius, Math.PI*2, false);
+    ctx.fillStyle = "0095DD"; 
+    ctx.fill(); 
+    ctx.closePath();
+}
+function draw() {
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    
+    drawBall();
 
-*/
+    x+=dx;
+    y+=dy;
+    if (x>canvas.width || x<0) {
+        dx = -dx;
+    }
+    if(y>canvas.height || y<0) {
+        dy = -dy;
+    }
+}
+setInterval(draw,10);
+
